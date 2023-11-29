7aefd1bb19cf908062a9219cf519fabbd8913f1f
hassamkhan92 <khan.hassam1992@gmail.com>
2023-11-29 22:59:20 +0500

    Refactoring Users Controller

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
+import { UsersController } from './controllers/users/users.controller';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
 })
 export class UsersModule {}
